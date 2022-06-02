df906643e0449d1673036028fd29e6b4746d035a
whodevil <whodevil@offthecob.info>
2022-06-02 11:43:56 -0700

    Refactor terraform out into reusable workflow
    I also cleaned up the other workflows with new learning. Added the
    concept of terraform workspaces to the project.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,8 @@
-#resource "aws_s3_bucket" "theagainagain_assets" {
-#  bucket = "theagainagain-assets"
-#  acl    = "public-read"
-#}
-#
+resource "aws_s3_bucket" "theagainagain_ui" {
+  bucket = "theagainagain-${terraform.workspace}"
+}
+
+resource "aws_s3_bucket_acl" "theagainagain_ui" {
+  bucket = aws_s3_bucket.theagainagain_ui.id
+  acl    = "public-read"
+}
