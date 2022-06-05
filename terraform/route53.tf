d47f6044045a55609618b11a6b2c93ef5fd67b34
whodevil <whodevil@offthecob.info>
2022-06-05 13:49:14 -0700

    Setup cloudfront

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -8,8 +8,8 @@ resource "aws_route53_record" "www" {
   name    = local.domain_name
   type    = "A"
   alias {
-    name = aws_s3_bucket.website.website_domain
-    zone_id = aws_s3_bucket.website.hosted_zone_id
+    name = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
