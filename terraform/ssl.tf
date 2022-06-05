bf4f0ba347d02bdc1b0141ebb2b6218ef11cd073
whodevil <whodevil@offthecob.info>
2022-06-05 14:02:26 -0700

    Setup cloudfront (#102)

---
+++ terraform/ssl.tf
@@ -0,0 +1,16 @@
+resource "aws_acm_certificate" "ssl_certificate" {
+  domain_name = local.domain_name
+  subject_alternative_names = ["*.${local.domain_name}"]
+  validation_method = "EMAIL"
+  #validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+  #validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
