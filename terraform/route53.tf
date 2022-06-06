6e12f02bb0bcac1604eca436c9dec4ecff0a17fd
whodevil <whodevil@offthecob.info>
2022-06-05 19:30:30 -0700

    Add additional details to cert validation (#104)

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,6 +7,7 @@ resource "aws_route53_record" "www" {
   zone_id = var.ZONE_ID
   name    = local.domain_name
   type    = "A"
+  allow_overwrite = true
   alias {
     name = aws_cloudfront_distribution.www_s3_distribution.domain_name
     zone_id = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
