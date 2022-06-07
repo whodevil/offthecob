eb610d0b3122bc1f7747d7db266c0916d263dc84
whodevil <whodevil@offthecob.info>
2022-06-07 16:27:39 -0700

    Add dns and ssl to apigateway
    I also changed the route to be /graphql

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,8 +1,3 @@
-locals {
-  domain_name_prefix = terraform.workspace == "production" ? "www" : terraform.workspace
-  domain_name        = "${local.domain_name_prefix}.offthecob.info"
-}
-
 resource "aws_route53_record" "www" {
   zone_id         = var.ZONE_ID
   name            = local.domain_name
@@ -15,6 +10,18 @@ resource "aws_route53_record" "www" {
   }
 }
 
+resource "aws_route53_record" "api" {
+  name    = aws_api_gateway_domain_name.api.domain_name
+  type    = "A"
+  zone_id = var.ZONE_ID
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
+  }
+}
+
 resource "aws_route53_record" "validation" {
   for_each = {
     for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
@@ -22,6 +29,23 @@ resource "aws_route53_record" "validation" {
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.ZONE_ID
+}
+
+resource "aws_route53_record" "api_validation" {
+  for_each = {
+  for dvo in aws_acm_certificate.api_ssl_certificate.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
   }
 
   allow_overwrite = true
