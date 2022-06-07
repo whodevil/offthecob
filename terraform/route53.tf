77e535fa592fb6e85cbb43df6b41c612bf7d77c5
whodevil <whodevil@offthecob.info>
2022-06-07 16:40:41 -0700

    Fix DNS, I'm not using CF for the api

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -17,8 +17,8 @@ resource "aws_route53_record" "api" {
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
+    name                   = aws_api_gateway_domain_name.api.domain_name
+    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
   }
 }
 
--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -16,7 +16,6 @@ resource "aws_acm_certificate_validation" "cert_validation" {
 }
 
 resource "aws_acm_certificate" "api_ssl_certificate" {
-  provider = aws.acm_provider
   domain_name               = local.api_name
   subject_alternative_names = ["*.${local.api_name}", local.api_name]
   validation_method         = "DNS"
@@ -27,7 +26,6 @@ resource "aws_acm_certificate" "api_ssl_certificate" {
 }
 
 resource "aws_acm_certificate_validation" "api_cert_validation" {
-  provider = aws.acm_provider
   certificate_arn         = aws_acm_certificate.api_ssl_certificate.arn
   validation_record_fqdns =  [for record in aws_route53_record.api_validation : record.fqdn]
 }
