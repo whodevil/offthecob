81ac8c70ce7d297fee8b69f9b49d596b5d0bb03f
whodevil <whodevil@offthecob.info>
2022-06-07 18:02:12 -0700

    wip

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,30 +1,30 @@
-#module "acm_www" {
-#  providers = {
-#    aws = aws.us-east-1
-#  }
-#  source  = "terraform-aws-modules/acm/aws"
-#  version = "~> 3.0"
-#
-#  wait_for_validation = true
-#
-#  domain_name               = local.domain_name
-#  zone_id                   = var.ZONE_ID
-#  subject_alternative_names = ["*.${local.domain_name}", local.domain_name]
-#  tags = {
-#    environment = terraform.workspace
-#  }
-#}
+module "acm_www" {
+  providers = {
+    aws = aws.us-east-1
+  }
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 3.0"
 
-#module "acm" {
-#  source  = "terraform-aws-modules/acm/aws"
-#  version = "~> 3.0"
-#
-#  wait_for_validation = true
-#
-#  domain_name               = local.api_name
-#  zone_id                   = var.ZONE_ID
-#  subject_alternative_names = ["*.${local.api_name}", local.api_name]
-#  tags = {
-#    environment = terraform.workspace
-#  }
-#}
+  wait_for_validation = true
+
+  domain_name               = local.domain_name
+  zone_id                   = var.ZONE_ID
+  subject_alternative_names = ["*.${local.domain_name}", local.domain_name]
+  tags = {
+    environment = terraform.workspace
+  }
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 3.0"
+
+  wait_for_validation = true
+
+  domain_name               = local.api_name
+  zone_id                   = var.ZONE_ID
+  subject_alternative_names = ["*.${local.api_name}", local.api_name]
+  tags = {
+    environment = terraform.workspace
+  }
+}
