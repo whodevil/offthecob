0018990c8b10295d1c107901c949bcf48b3fa3df
whodevil <whodevil@offthecob.info>
2022-06-08 13:51:33 -0700

    Take another crack at api gateway

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -26,3 +26,15 @@ resource "aws_route53_record" "validation" {
   type            = each.value.type
   zone_id         = var.ZONE_ID
 }
+
+resource "aws_route53_record" "api" {
+  name    = aws_apigatewayv2_domain_name.api.domain_name
+  type    = "A"
+  zone_id = var.ZONE_ID
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
