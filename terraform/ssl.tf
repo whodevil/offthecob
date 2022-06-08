05d98fb2f47321b9c7c95d7352396e2b75f21998
whodevil <whodevil@offthecob.info>
2022-06-07 18:50:28 -0700

    revert to not using acm module

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,18 +1,18 @@
-module "acm_www" {
-  providers = {
-    aws = aws.us-east-1
-  }
-  source  = "terraform-aws-modules/acm/aws"
-  version = "~> 3.0"
-
-  wait_for_validation = true
-
+resource "aws_acm_certificate" "ssl_certificate" {
+  provider = aws.acm_provider
   domain_name               = local.domain_name
-  zone_id                   = var.ZONE_ID
   subject_alternative_names = ["*.${local.domain_name}", local.domain_name]
-  tags = {
-    environment = terraform.workspace
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  provider = aws.acm_provider
+  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns =  [for record in aws_route53_record.validation : record.fqdn]
 }
 
 module "acm" {
