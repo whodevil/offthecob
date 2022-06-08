cccce6c8ed504f45d273eac2c8e5058d4a01df7b
whodevil <whodevil@offthecob.info>
2022-06-07 17:47:44 -0700

    wip

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,30 +1,30 @@
-resource "aws_acm_certificate" "ssl_certificate" {
-  provider = aws.acm_provider
-  domain_name               = local.domain_name
-  subject_alternative_names = ["*.${local.domain_name}", local.domain_name]
-  validation_method         = "DNS"
+#module "acm_www" {
+#  providers = {
+#    aws = aws.us-east-1
+#  }
+#  source  = "terraform-aws-modules/acm/aws"
+#  version = "~> 3.0"
+#
+#  wait_for_validation = true
+#
+#  domain_name               = local.domain_name
+#  zone_id                   = var.ZONE_ID
+#  subject_alternative_names = ["*.${local.domain_name}", local.domain_name]
+#  tags = {
+#    environment = terraform.workspace
+#  }
+#}
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider = aws.acm_provider
-  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns =  [for record in aws_route53_record.validation : record.fqdn]
-}
-
-module "acm" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "~> 3.0"
-
-  wait_for_validation = true
-
-  domain_name               = local.api_name
-  zone_id                   = var.ZONE_ID
-  subject_alternative_names = ["*.${local.api_name}", local.api_name]
-  tags = {
-    environment = terraform.workspace
-  }
-}
+#module "acm" {
+#  source  = "terraform-aws-modules/acm/aws"
+#  version = "~> 3.0"
+#
+#  wait_for_validation = true
+#
+#  domain_name               = local.api_name
+#  zone_id                   = var.ZONE_ID
+#  subject_alternative_names = ["*.${local.api_name}", local.api_name]
+#  tags = {
+#    environment = terraform.workspace
+#  }
+#}
