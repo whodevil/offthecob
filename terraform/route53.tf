d3d9852393ecc276ad6fda139999769584799985
whodevil <whodevil@offthecob.info>
2022-06-08 17:43:29 -0700

    Publish lambda to fix bug, and terraform fmt

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -12,11 +12,11 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "validation" {
   for_each = {
-  for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  }
+    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
 
   allow_overwrite = true
--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "ssl_certificate" {
-  provider = aws.us-east-1
+  provider                  = aws.us-east-1
   domain_name               = local.domain_name
   subject_alternative_names = ["*.${local.domain_name}", local.domain_name]
   validation_method         = "DNS"
@@ -10,9 +10,9 @@ resource "aws_acm_certificate" "ssl_certificate" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  provider = aws.us-east-1
+  provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns =  [for record in aws_route53_record.validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
 
 module "acm" {
