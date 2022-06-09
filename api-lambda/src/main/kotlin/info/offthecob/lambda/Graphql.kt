08e1804ce750e243a98f4b2557a0904da0bb8cfc
whodevil <whodevil@offthecob.info>
2022-06-08 17:59:15 -0700

    revert events change to see what broke

--- api-lambda/src/main/kotlin/info/offthecob/lambda/Graphql.kt
+++ api-lambda/src/main/kotlin/info/offthecob/lambda/Graphql.kt
@@ -2,27 +2,18 @@ package info.offthecob.lambda
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
-class Graphql : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
-
-    override fun handleRequest(input: APIGatewayV2HTTPEvent?, context: Context?): APIGatewayV2HTTPResponse {
-        logger.info { "input: ${input?.requestContext?.http?.method}" }
-        logger.info { "body: ${input?.body}"}
-        val response = APIGatewayV2HTTPResponse()
-        when(input?.requestContext?.http?.method) {
-            "POST" -> {
-                response.body = "success"
-                response.statusCode = 200
+class Graphql : RequestHandler<Any, String> {
+    override fun handleRequest(input: Any?, context: Context?): String {
+        return when (input!!) {
+            is String -> return "hello $input"
+            is Map<*, *> -> {
+                logger.info { "###### input: $input" }
+                return "hello Map"
             }
-            else -> {
-                response.body = "not sure what happened"
-                response.statusCode = 400
-            }
+            else -> "unsupported"
         }
-        return response
     }
 }
