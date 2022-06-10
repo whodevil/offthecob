4863adc2021ddb23c878fb6bd88f67425a9e4e33
whodevil <whodevil@offthecob.info>
2022-06-09 19:41:49 -0700

    More wrangling with the graphiql (#141)

--- ui/src/GraphqlWrapper.tsx
+++ ui/src/GraphqlWrapper.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import {GraphiQL} from "graphiql";
-import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import {GraphiQL} from "graphiql"
+import {createGraphiQLFetcher} from '@graphiql/toolkit'
+import 'graphiql/graphiql.min.css'
 
 const fetcher = createGraphiQLFetcher({
     url: 'https://api-staging.offthecob.info/graphql',
+    headers: {
+        'X-Requested-With': 'graphiql',
+    }
 });
 
 const GraphqlWrapper: React.FC = () => {
     return (
-        <GraphiQL fetcher={fetcher} />
+        <GraphiQL fetcher={fetcher}/>
     )
 }
 
